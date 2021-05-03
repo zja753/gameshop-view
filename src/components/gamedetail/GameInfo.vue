8451cff3e44a5f468e71765b9a66231816eaeccf
zja753 <707092956@qq.com>
2021-05-03 18:38:35 +0800

    feat: 订单详情页

--- src/components/gamedetail/GameInfo.vue
+++ src/components/gamedetail/GameInfo.vue
@@ -78,6 +78,7 @@ export default {
         })
         .then((res) => {
           console.log(res);
+          this.$router.push(`/orderInfo/${res.data._id}`);
         });
     },
   },
--- src/views/OrderInfo.vue
+++ src/views/OrderInfo.vue
@@ -2,12 +2,28 @@
   <div class="gamelist">
     <main>
       <el-table :data="tableData" stripe style="width: 100%">
-        <el-table-column prop="date" label="日期" width="180">
+        <el-table-column prop="group_name" label="游戏分组" width="180">
         </el-table-column>
-        <el-table-column prop="name" label="姓名" width="180">
+        <el-table-column prop="name" label="游戏名" width="180">
         </el-table-column>
-        <el-table-column prop="address" label="地址"> </el-table-column>
+        <el-table-column prop="brief_introduction" label="简介">
+        </el-table-column>
+        <el-table-column prop="price" label="价格"> </el-table-column>
+        <el-table-column prop="is_dlc" label="是否DLC">
+          <template slot-scope="scope">
+            {{ scope.row.is_dlc ? "是" : "否" }}
+          </template>
+        </el-table-column>
       </el-table>
+
+      <div class="fooer">
+        游戏总数:{{ this.order && this.order.quantity }} 总价格:{{
+          this.order && this.order.totle_price
+        }}元
+        <el-button class="buyBtn" type="success" round @click="payOrder">
+          支付订单
+        </el-button>
+      </div>
     </main>
   </div>
 </template>
@@ -19,35 +35,47 @@ export default {
     return {
       tableData: [
         {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1517 弄",
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1519 弄",
-        },
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1516 弄",
+          group_name: "中土世界™",
+          name: "中土世界™ : 战争之影™ 决定版",
+          brief_introduction:
+            "包含 6 件物品： Middle-earth™: Shadow of War™, Middle-earth™: Shadow of War™ Story Expansion Pass, Outlaw Tribe Nemesis Expansion, Slaughter Tribe Nemesis Expansion, The Blade of Galadriel Story Expansion, The Desolation of Mordor Story Expansion",
+          price: "163.0",
+          is_dlc: false,
         },
       ],
+      order: null,
     };
   },
   components: {},
   created() {
+    this.order_id = this.$route.params.id;
     this.getOrderInfo();
   },
   mounted() {},
   methods: {
-    getOrderInfo() {},
+    getOrderInfo() {
+      this.$axios.get("order/get", { _id: this.order_id }).then((res) => {
+        const { data } = res;
+        this.tableData = data.productList;
+        this.order = data;
+      });
+    },
+    payOrder() {
+      //   console.log(this.order_id);
+      this.$axios.post("order/pay", { order_id: this.order_id }).then((res) => {
+        if (res.status === 1) {
+          this.$message({
+            message: res.msg,
+            type: "success",
+          });
+        } else {
+          this.$message({
+            message: res.err,
+            type: "warning",
+          });
+        }
+      });
+    },
   },
 };
 </script>
@@ -59,6 +87,20 @@ export default {
   main {
     width: 1200px;
     margin: auto;
+    min-height: calc(100vh - 184px);
+
+    .fooer {
+      width: 100%;
+      height: 50px;
+      color: #fff;
+      font-size: 22px;
+      text-align: right;
+      line-height: 50px;
+
+      .buyBtn {
+        margin-left: 20px;
+      }
+    }
   }
 }
 </style>
